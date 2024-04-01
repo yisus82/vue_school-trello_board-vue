dc3daeb4f318aaf0da998278a84a214775f3154b
yisus82 <djalma8@gmail.com>
2024-04-01 12:59:21 +0200

    Make columns draggable

--- trevuello/components/TrelloBoard.vue
+++ trevuello/components/TrelloBoard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { Column } from '@/types';
 import { nanoid } from 'nanoid';
+import draggable from 'vuedraggable';
 
 const columns = ref<Column[]>([
   {
@@ -27,19 +28,33 @@ const columns = ref<Column[]>([
       { id: nanoid(), title: 'Task 6' },
     ],
   },
+  {
+    id: nanoid(),
+    title: 'Archived',
+    tasks: [
+      { id: nanoid(), title: 'Task 7' },
+      { id: nanoid(), title: 'Task 8' },
+      { id: nanoid(), title: 'Task 9' },
+      { id: nanoid(), title: 'Task 10' }
+    ],
+  },
 ]);
 </script>
 
 <template>
-  <div class="flex gap-4 overflow-x-auto items-start">
-    <div v-for="column in columns" :key="column.id" class="bg-gray-200 p-5 rounded min-w-[250px]">
-      <header class="mb-4">
-        <h2 class="font-bold">{{ column.title }}</h2>
-      </header>
-      <TrelloBoardTask v-for="task in column.tasks" :key="task.id" :task="task" />
-      <footer>
-        <button class="text-gray-500">+ Add Task</button>
-      </footer>
-    </div>
+  <div>
+    <draggable class="flex gap-4 overflow-x-auto items-start" v-model="columns" group="columns" item-key="id">
+      <template #item="{ element: column }: { element: Column; }">
+        <div class="bg-gray-200 p-5 rounded min-w-[250px]">
+          <header class="mb-4">
+            <h2 class="font-bold">{{ column.title }}</h2>
+          </header>
+          <TrelloBoardTask v-for="task in column.tasks" :key="task.id" :task="task" />
+          <footer>
+            <button class="text-gray-500">+ Add Task</button>
+          </footer>
+        </div>
+      </template>
+    </draggable>
   </div>
 </template>
