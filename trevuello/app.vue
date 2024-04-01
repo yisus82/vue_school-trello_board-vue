63a6b85b32f5da6607c68a31933e541d2399e830
yisus82 <djalma8@gmail.com>
2024-04-01 02:49:48 +0200

    Add some styles to the board

--- trevuello/app.vue
+++ trevuello/app.vue
@@ -1,3 +1,5 @@
 <template>
-  <TrelloBoard />
+  <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
+    <TrelloBoard />
+  </div>
 </template>
--- trevuello/components/TrelloBoard.vue
+++ trevuello/components/TrelloBoard.vue
@@ -1,38 +1,38 @@
 <script setup lang="ts">
-  import type { Column } from '@/types';
-  import { nanoid } from 'nanoid';
+import type { Column } from '@/types';
+import { nanoid } from 'nanoid';
 
-  const columns = ref<Column[]>([
-    {
-      id: nanoid(),
-      title: 'To Do',
-      tasks: [
-        { id: nanoid(), title: 'Task 1' },
-        { id: nanoid(), title: 'Task 2' },
-      ],
-    },
-    {
-      id: nanoid(),
-      title: 'In Progress',
-      tasks: [
-        { id: nanoid(), title: 'Task 3' },
-        { id: nanoid(), title: 'Task 4' },
-      ],
-    },
-    {
-      id: nanoid(),
-      title: 'Done',
-      tasks: [
-        { id: nanoid(), title: 'Task 5' },
-        { id: nanoid(), title: 'Task 6' },
-      ],
-    },
-  ]);
+const columns = ref<Column[]>([
+  {
+    id: nanoid(),
+    title: 'To Do',
+    tasks: [
+      { id: nanoid(), title: 'Task 1' },
+      { id: nanoid(), title: 'Task 2' },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: 'In Progress',
+    tasks: [
+      { id: nanoid(), title: 'Task 3' },
+      { id: nanoid(), title: 'Task 4' },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: 'Done',
+    tasks: [
+      { id: nanoid(), title: 'Task 5' },
+      { id: nanoid(), title: 'Task 6' },
+    ],
+  },
+]);
 </script>
 
 <template>
-  <div>
-    <div v-for="column in columns" :key="column.id">
+  <div class="flex gap-4 overflow-x-auto items-start">
+    <div v-for="column in columns" :key="column.id" class="bg-gray-200 p-5 rounded min-w-[250px]">
       <h2>{{ column.title }}</h2>
       <p v-for="task in column.tasks" :key="task.id">
         {{ task.title }}
